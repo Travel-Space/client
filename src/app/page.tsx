400c76842664e2e5b7890ddb9989eb5cc3d61d0a
helloleesul <55569192+helloleesul@users.noreply.github.com>
2023-10-09 23:45:01 +0900

    fix: layout 글로벌 스타일 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,7 @@
-"use client";
-import GlobalStyles from "@/styles/global";
-import theme from "@/styles/theme";
-import { ThemeProvider } from "styled-components";
-
-export default function App() {
-  const a: String = "abe";
-
+export default function Home() {
   return (
-    <>
-      <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <main>
-          그럼이건모지? 괜찮군 브랜치를 따보자<a href="/post">post</a>
-        </main>
-      </ThemeProvider>
-    </>
+    <main>
+      그럼이건모지? 괜찮군 브랜치를 따보자<a href="/post">post</a>
+    </main>
   );
 }
