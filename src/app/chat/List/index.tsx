bcf80ea3b588ff81e645c4bd4680cf3d399b7d4e
joayeon <codinz.n@gmail.com>
2023-10-11 04:57:38 +0900

    design: chatting list 디자인 작업 진행 중

--- src/app/chat/List/index.tsx
+++ src/app/chat/List/index.tsx
@@ -2,9 +2,25 @@ import * as S from "./index.styled";
 
 export default function ChatList() {
   return (
-    <S.Box>
-      <div>function</div>
-      <div>line</div>
-    </S.Box>
+    <>
+      <S.ListBox>
+        <S.ChatRoomImg>
+          <S.Image src="/assets/img/icons/profile.svg" />
+        </S.ChatRoomImg>
+
+        <S.ChatRoom>
+          <S.ChatRoomTitle>
+            <span>행성 : 일본 도쿄</span>
+            <img src="/assets/img/icons/new-chat.svg" />
+          </S.ChatRoomTitle>
+
+          <S.ChatPreview>
+            <span>메시지 뜨는 칸 메시지 뜨는 칸 메시지 뜨는 칸 메시지 뜨는 칸</span>
+          </S.ChatPreview>
+        </S.ChatRoom>
+      </S.ListBox>
+
+      <S.Line />
+    </>
   );
 }
