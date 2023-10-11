dc5a0f42b297347bd2e2d823d8905b7fef3f2581
[SW5] 문수민 <sum95771@gmail.com>
2023-10-12 01:15:51 +0900

    design: Nav 컴포넌트 디자인 추가

--- src/app/my/Nav/index.styled.ts
+++ src/app/my/Nav/index.styled.ts
@@ -5,4 +5,5 @@ export const Container = styled.div`
   border: 1px solid ${({ theme }) => theme.PALETTE.gray[100]};
   border-radius: 10px;
   padding-bottom: 16px;
+  background-color: ${({ theme }) => theme.PALETTE.white};
 `;
