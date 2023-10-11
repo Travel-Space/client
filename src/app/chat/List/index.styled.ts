734830c5eebb51bce81c5930cfb0558447bceba1
joayeon <codinz.n@gmail.com>
2023-10-11 23:58:54 +0900

    design: chat list design

--- src/app/chat/List/index.styled.ts
+++ src/app/chat/List/index.styled.ts
@@ -1,17 +1,21 @@
-import { flexAlignCenter, flexCenter, flexColumn, flexSpaceBetweenCenter } from "@/styles/common";
 import styled from "styled-components";
+import { flexCenter, flexColumn } from "@/styles/common";
 
 export const ListBox = styled.div`
-  ${flexCenter}
   padding: 24px 32px;
+  ${flexCenter}
   gap: 16px;
+
+  > div {
+    cursor: pointer;
+  }
 `;
 
-export const ChatRoomImg = styled.div``;
-
-export const Image = styled.img`
-  width: 50px;
-  height: 50px;
+export const ChatRoomImg = styled.div`
+  > img {
+    width: 50px;
+    height: 50px;
+  }
 `;
 
 export const ChatRoom = styled.div`
@@ -30,16 +34,10 @@ export const ChatRoomTitle = styled.div`
 export const ChatPreview = styled(ChatRoomTitle)`
   font-family: ${({ theme }) => theme.FONT_WEIGHT.regular};
 
-  & span {
-    white-space: nowrap; /* 텍스트를 한 줄에 표시 */
-    overflow: hidden; /* 넘칠 경우 숨김 처리 */
-    text-overflow: ellipsis; /* 넘칠 경우 "..."으로 표시 */
+  > span {
     width: 240px; /* 표시할 너비 설정 */
+    overflow: hidden; /* 넘칠 경우 숨김 처리 */
+    white-space: nowrap; /* 텍스트를 한 줄에 표시 */
+    text-overflow: ellipsis; /* 넘칠 경우 "..."으로 표시 */
   }
-`;
-
-export const Line = styled.div`
-  height: 1px;
-  border: 1px solid #d9d9d9;
-  width: 100%;
 `;
