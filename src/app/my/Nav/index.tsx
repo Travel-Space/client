334b1160b6578a52c115ddfbac055ad2a62e8fb0
[SW5] 문수민 <sum95771@gmail.com>
2023-10-12 01:16:39 +0900

    feat: 마이페이지 navigation 경로 설정

--- src/app/my/Nav/index.tsx
+++ src/app/my/Nav/index.tsx
@@ -10,15 +10,52 @@ export default function Nav() {
     <S.Container>
       <Profile imgSrc={"/assets/img/icons/default-user.svg"} nickname={"곰숨곰숨짱"} email={"aaaa1234@gmail.com"} />
       <Divider />
-      <NavList logo={"/assets/img/icons/statistics-management.svg"} title={"통계"} list={["행성 통계"]} />
+      <NavList
+        href="/my/statistics"
+        logo={"/assets/img/icons/statistics-management.svg"}
+        title={"통계"}
+        list={[{ href: "/my/statistics", title: "행성 통계" }]}
+      />
       <Divider />
-      <NavList logo={"/assets/img/icons/default-user.svg"} title={"기본 정보 관리"} list={["프로필", "행성"]} />
+      <NavList
+        href="/my/profile"
+        logo={"/assets/img/icons/default-user.svg"}
+        title={"기본 정보 관리"}
+        list={[
+          { href: "/my/profile", title: "프로필" },
+          { href: "/my/planet", title: "행성" },
+        ]}
+      />
       <Divider />
-      <NavList logo={"/assets/img/icons/contents-management.svg"} title={"컨텐츠 관리"} list={["게시글", "댓글"]} />
+      <NavList
+        href="/my/postings"
+        logo={"/assets/img/icons/contents-management.svg"}
+        title={"컨텐츠 관리"}
+        list={[
+          { href: "/my/postings", title: "게시글" },
+          { href: "/my/comments", title: "댓글" },
+        ]}
+      />
       <Divider />
-      <NavList logo={"/assets/img/icons/friend-management.svg"} title={"친구 관리"} list={["친구 목록", "친구 추가"]} />
+      <NavList
+        href="/my/friends"
+        logo={"/assets/img/icons/friend-management.svg"}
+        title={"친구 관리"}
+        list={[
+          { href: "/my/friends", title: "친구 목록" },
+          { href: "/my/addFriend", title: "친구 추가" },
+        ]}
+      />
       <Divider />
-      <NavList logo={"/assets/img/icons/like-management.svg"} title={"좋아요 관리"} list={["행성", "게시글"]} />
+      <NavList
+        href="/my/planetLike"
+        logo={"/assets/img/icons/like-management.svg"}
+        title={"좋아요 관리"}
+        list={[
+          { href: "/my/planetLike", title: "행성" },
+          { href: "/my/postingLike", title: "게시글" },
+        ]}
+      />
     </S.Container>
   );
 }
