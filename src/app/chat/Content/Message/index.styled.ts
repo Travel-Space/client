94a38a75c0432eca89db364bb5a670ced410fc97
joayeon <codinz.n@gmail.com>
2023-10-12 03:35:58 +0900

    design: message 본문 디자인

--- src/app/chat/Content/Message/index.styled.ts
+++ src/app/chat/Content/Message/index.styled.ts
@@ -1,9 +1,26 @@
-import { flexAlignCenter } from "@/styles/common";
+import { flexAlignCenter, flexCenter, flexColumn } from "@/styles/common";
 import styled from "styled-components";
 
+export const Body = styled.div`
+  height: 685px;
+  overflow: auto; /* 세로 스크롤을 활성화 */
+  padding: 12px 0 12px 0;
+`;
+
 export const Message = styled.div`
-  ${flexAlignCenter};
+  ${flexCenter};
+  justify-content: ${props => (props.message === "my" || props.message === "photo" ? "flex-end" : "flex-start")};
   gap: 8px;
+  width: 100%;
+`;
+
+export const PhotoMessage = styled(Message)`
+  align-items: flex-end;
+`;
+
+export const MessageBox = styled.div`
+  ${flexColumn}
+  gap: 16px;
 `;
 
 export const Image = styled.img`
@@ -11,4 +28,42 @@ export const Image = styled.img`
   height: 56px;
 `;
 
-export const Intro = styled.div``;
+export const Info = styled.div`
+  ${flexColumn}
+  gap: 8px;
+`;
+
+export const Intro = styled.div`
+  display: flex;
+  align-items: flex-end;
+  gap: 8px;
+
+  > div {
+    font-size: ${({ theme }) => theme.FONT_SIZE.xs};
+  }
+`;
+
+export const Nickname = styled.div`
+  font-size: ${({ theme }) => theme.FONT_SIZE.sm};
+  font-family: ${({ theme }) => theme.FONT_WEIGHT.bold};
+`;
+
+export const TextContent = styled.span`
+  background: #fff;
+  padding: 10px 24px;
+  ${flexAlignCenter}
+  border-radius: 20px;
+  border: 1px solid #d9d9d9;
+  color: ${({ theme }) => theme.PALETTE.black};
+  font-size: ${({ theme }) => theme.FONT_SIZE.sm};
+`;
+
+export const Images = styled.div`
+  > img {
+    width: 200px;
+    height: 200px;
+    object-fit: cover;
+    border-radius: 10px;
+    border: 5px solid #fff;
+  }
+`;
--- src/app/chat/Content/Message/index.tsx
+++ src/app/chat/Content/Message/index.tsx
@@ -2,27 +2,50 @@ import * as S from "./index.styled";
 
 export default function Message() {
   return (
-    <div>
+    <S.Body>
       {/* 날짜가 변경될 때마다 표시되어야 할 선 */}
 
-      {/* 내가 보낸 메시지 */}
-      <MyMessage />
+      <S.MessageBox>
+        {/* 내가 보낸 메시지 */}
+        <MyMessage />
+        <MyMessage />
+        <MyMessage />
+        <MyMessage />
+        <MyMessage />
+        <MyMessage />
+        <PhotoMessage />
 
-      {/* 남이 보낸 메시지 */}
-      <SomeoneMessage />
-    </div>
+        {/* 남이 보낸 메시지 */}
+        <SomeoneMessage />
+        <SomeoneMessage />
+      </S.MessageBox>
+    </S.Body>
   );
 }
 
+const PhotoMessage = () => {
+  return (
+    <S.PhotoMessage message={"photo"}>
+      <S.Intro>
+        <div>23:23</div>
+        <S.Images>
+          <img src="/assets/img/background/background-3.png" />
+        </S.Images>
+      </S.Intro>
+      <div>
+        <S.Image src="/assets/img/icons/profileImage.png" />
+      </div>
+    </S.PhotoMessage>
+  );
+};
+
 const MyMessage = () => {
   return (
-    <S.Message>
-      <div>
-        <div>
-          <span>야 오늘 일본 날씨 대박이더라</span>
-        </div>
+    <S.Message message={"my"}>
+      <S.Intro>
         <div>23:23</div>
-      </div>
+        <S.TextContent>야 오늘 일본 날씨 대박이더라</S.TextContent>
+      </S.Intro>
       <div>
         <S.Image src="/assets/img/icons/profileImage.png" />
       </div>
@@ -32,17 +55,17 @@ const MyMessage = () => {
 
 const SomeoneMessage = () => {
   return (
-    <S.Message>
+    <S.Message message={"someone"}>
       <div>
         <S.Image src="/assets/img/icons/profileImage.png" />
       </div>
-      <div>
-        <div>닉네임이들어갈자리</div>
-        <div>
-          <div>그니까 오늘 일본 날씨 미쳤더라 또 가고 싶네</div>
+      <S.Info>
+        <S.Nickname>닉네임이 들어갈 자리</S.Nickname>
+        <S.Intro>
+          <S.TextContent>그니까 오늘 일본 날씨 미쳤더라 또 가고 싶네</S.TextContent>
           <div>23:24</div>
-        </div>
-      </div>
+        </S.Intro>
+      </S.Info>
     </S.Message>
   );
 };
