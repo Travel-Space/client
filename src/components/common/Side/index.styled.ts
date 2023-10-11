f4881ea4e505f00b2e49fc6772cb51e2d7409ede
helloleesul <55569192+helloleesul@users.noreply.github.com>
2023-10-11 22:04:59 +0900

    design: Side 컴포넌트 슬라이드 트랜지션 추가

--- src/components/common/Side/index.styled.ts
+++ src/components/common/Side/index.styled.ts
@@ -1,15 +1,31 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { ModalBackground } from "@/styles/common";
 
 export const Background = styled.div`
   ${ModalBackground}
 `;
 
+const WIDTH = "443px";
+
+const slide = keyframes`
+  from {
+    right: -${WIDTH};
+  }
+  to {
+    right: 0;
+  }
+`;
+
 export const Container = styled.div`
   position: absolute;
   right: 0;
   top: 0;
-  width: 443px;
+  width: ${WIDTH};
   background-color: ${({ theme }) => theme.PALETTE.white};
   height: 100%;
+
+  animation-duration: 0.25s;
+  animation-timing-function: ease-out;
+  animation-name: ${slide};
+  animation-fill-mode: forwards;
 `;
