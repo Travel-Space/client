762088a362a00729b3d473fb24cffd5c92ab3f7c
[SW5] 문수민 <sum95771@gmail.com>
2023-10-12 19:09:48 +0900

    design: MyPlanet 퍼블리싱

--- src/app/my-page/basic-info/planet/MyPlanet/index.styled.ts
+++ src/app/my-page/basic-info/planet/MyPlanet/index.styled.ts
@@ -1,17 +1,19 @@
 import { styled } from "styled-components";
-import { flexCenter, flexSpaceBetweenCenter } from "@/styles/common";
+import { flexSpaceBetweenCenter } from "@/styles/common";
 
 export const Container = styled.div`
   width: 152px;
   height: 186px;
   border: 1px solid ${({ theme }) => theme.PALETTE.gray[100]};
-  border-radius: 10px;
+  border-radius: 15px;
   background-color: ${({ theme }) => theme.PALETTE.white};
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
   padding: 16px;
   ${flexSpaceBetweenCenter}
   flex-direction: column;
 `;
 export const Header = styled.div`
+  width: 100%;
   ${flexSpaceBetweenCenter}
 `;
 export const People = styled.div`
@@ -19,4 +21,7 @@ export const People = styled.div`
   font-size: ${({ theme }) => theme.FONT_SIZE.xs};
   line-heifht: 20px;
 `;
-export const Title = styled.div``;
+export const Title = styled.div`
+  font-size: ${({ theme }) => theme.FONT_SIZE.sm};
+  font-family: ${({ theme }) => theme.FONT_WEIGHT.bold};
+`;
