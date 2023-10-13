a25d145e3518f6c2958c8c8f9b451f34cf0cfdc7
wook888 <guddnr888@gmail.com>
2023-10-13 22:45:22 +0900

    style: style: img를 div/background-url 로 지정한 부분 수정

--- src/app/home/PlanetList/index.styled.ts
+++ src/app/home/PlanetList/index.styled.ts
@@ -10,10 +10,7 @@ export const PlanetImg = styled.img`
   width: 200px;
   height: 200px;
 
-  background-image: url(/assets/img/icons/planet1.png);
   background-repeat: no-repeat;
   background-size: contain;
   background-position: center;
-
-  content-visibility: hidden;
 `;
