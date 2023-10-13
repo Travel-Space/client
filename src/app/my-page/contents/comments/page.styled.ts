e321395b677605e88959ef44371ec605d145af21
[SW5] 문수민 <sum95771@gmail.com>
2023-10-13 20:04:14 +0900

    design: 페이지 내 검색창 스타일 삭제(검색창 컴포넌트 분리)

--- src/app/my-page/contents/comments/page.styled.ts
+++ src/app/my-page/contents/comments/page.styled.ts
@@ -23,36 +23,3 @@ export const MyCommentsWrap = styled.div`
   border: 1px solid ${({ theme }) => theme.PALETTE.gray[100]};
   border-radius: 10px;
 `;
-//
-export const Search = styled.div`
-  display: flex;
-  align-items: center;
-  width: 323px;
-  height: 30px;
-  padding: 5px 16px;
-  display: flex;
-  gap: 4px;
-  border: 1px solid ${({ theme }) => theme.PALETTE.gray[100]};
-  border-radius: 10px;
-`;
-export const Filter = styled.div`
-  font-family: ${({ theme }) => theme.FONT_WEIGHT.regular};
-  font-size: ${({ theme }) => theme.FONT_SIZE.sm};
-`;
-export const VerticalDivider = styled.div`
-  width: 1px;
-  height: 12px;
-  margin: 0 8px;
-  background-color: ${({ theme }) => theme.PALETTE.gray[100]};
-`;
-export const SearchInput = styled.input`
-  border: none;
-  font-family: ${({ theme }) => theme.FONT_WEIGHT.regular};
-  font-size: ${({ theme }) => theme.FONT_SIZE.sm};
-
-  &::placeholder {
-    font-family: ${({ theme }) => theme.FONT_WEIGHT.regular};
-    font-size: ${({ theme }) => theme.FONT_SIZE.sm};
-    color: ${({ theme }) => theme.PALETTE.gray[100]};
-  }
-`;
