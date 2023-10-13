38ec43b037e8ad8f57db6821d81f7d30c5322d4f
[SW5] 문수민 <sum95771@gmail.com>
2023-10-13 15:20:49 +0900

    rename: 검색창 컴포넌트 파일 위치 변경

--- src/app/my-page/contents/postings/page.tsx
+++ src/app/my-page/contents/postings/page.tsx
@@ -4,6 +4,7 @@ import * as S from "./page.styled";
 
 import NoPostings from "./NoPostings";
 import MyPostings from "./MyPostings";
+import SearchForm from "../../SearchForm";
 
 export default function Postings() {
   return (
@@ -14,11 +15,7 @@ export default function Postings() {
         <S.PostingsNumber>
           총 <span>30</span>개의 게시글
         </S.PostingsNumber>
-        <S.Search>
-          <S.Filter>글제목 ▾</S.Filter>
-          <S.VerticalDivider />
-          <S.SearchInput type="text" placeholder={`게시글 관리에서 검색합니다.`} />
-        </S.Search>
+        <SearchForm />
       </S.Header>
       <S.MyPostingsWrap>
         <MyPostings />
