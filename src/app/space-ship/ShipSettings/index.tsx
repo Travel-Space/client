22eb8d678b592bf0516bad28da04e002450e8bca
helloleesul <55569192+helloleesul@users.noreply.github.com>
2023-10-16 17:54:33 +0900

    design: 행성 탈출(나가기) 모달 퍼블리싱 완료

--- src/app/space-ship/ShipSettings/index.tsx
+++ src/app/space-ship/ShipSettings/index.tsx
@@ -1,13 +1,19 @@
+import { useState } from "react";
 import * as S from "./index.styled";
+import ExitPlanetModal from "../ExitPlanetModal";
 
 export default function ShipsSettings() {
+  const [showExitPlanetModal, setShowExitPlanetModal] = useState(false);
   return (
     <S.Wrap>
       <S.LinkButton>
         <img src="/assets/img/icons/users.svg" />
         행성 멤버 관리
       </S.LinkButton>
-      <S.ExitButton>행성 탈출 💥</S.ExitButton>
+      <S.ExitButton onClick={() => setShowExitPlanetModal(true)}>행성 탈출 💥</S.ExitButton>
+      {showExitPlanetModal ? (
+        <ExitPlanetModal onClose={() => setShowExitPlanetModal(false)} planetTitle="일본 맛도리 여행" />
+      ) : null}
     </S.Wrap>
   );
 }
