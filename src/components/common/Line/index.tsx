21e2853fb01f1c63a208181c1f27ca5abae7bd98
joayeon <codinz.n@gmail.com>
2023-10-17 21:49:41 +0900

    design: line 컴포넌트 css 수정

--- src/components/common/Line/index.tsx
+++ src/components/common/Line/index.tsx
@@ -2,8 +2,9 @@ import * as S from "./index.styled";
 
 export interface colorProp {
   color: "gray" | "white";
+  size: "vertical" | "horizontal";
 }
 
-export default function Line({ color }: colorProp) {
-  return <S.Line color={color} />;
+export default function Line({ color, size }: colorProp) {
+  return <S.Line color={color} size={size} />;
 }
