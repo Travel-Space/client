26c5e9c119ecb8f7eb2d7a64ab7af387dcb0d243
helloleesul <55569192+helloleesul@users.noreply.github.com>
2023-10-17 21:20:23 +0900

    design: 우주선 정보 모달 퍼블리싱

--- src/app/space-ship/ShipList/Ship/index.tsx
+++ src/app/space-ship/ShipList/Ship/index.tsx
@@ -1,19 +1,25 @@
 import { useState } from "react";
 import * as S from "./index.styled";
 import ManageShip from "../../Modal/ManageShip";
+import InfoShip from "../../Modal/InfoShip";
 
 export default function Ship({ test }: { test: number }) {
   const [newShipModal, setNewShipModal] = useState<boolean>(false);
+  const [infoShipModal, setInfoShipModal] = useState<boolean>(false);
   return (
     <S.Wrap>
       <S.Container>
-        <S.ShipImg onClick={() => setNewShipModal(true)}>
+        {/* <S.ShipImg onClick={() => setNewShipModal(true)}>
+          <img src="/assets/img/icons/ship-profile-create.svg" alt="" />
+        </S.ShipImg> */}
+        <S.ShipImg onClick={() => setInfoShipModal(true)}>
           <img src="/assets/img/icons/ship-profile-create.svg" alt="" />
         </S.ShipImg>
         <S.ShipTitle>우주선 이름 {test}</S.ShipTitle>
         <S.ShipMemberCount>1 / 10</S.ShipMemberCount>
       </S.Container>
       {newShipModal ? <ManageShip onClose={() => setNewShipModal(false)} /> : null}
+      {infoShipModal ? <InfoShip onClose={() => setInfoShipModal(false)} /> : null}
     </S.Wrap>
   );
 }
