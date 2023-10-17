7b77c3cc512e51c5b2aaccde2c16f02459d30260
helloleesul <55569192+helloleesul@users.noreply.github.com>
2023-10-17 19:26:20 +0900

    design: plus minus 버튼이 포함된 number input 공통 컴포넌트 생성

--- src/app/space-ship/Modal/ManageShip/index.styled.ts
+++ src/app/space-ship/Modal/ManageShip/index.styled.ts
@@ -1,3 +1,19 @@
+import { flexCenter } from "@/styles/common";
 import styled from "styled-components";
+import { InputGroup } from "@/components/Account/common.styled";
 
 export const Notification = styled.div``;
+
+export const Center = styled.div`
+  ${flexCenter}
+  gap: 24px;
+`;
+
+export const AdjustBtnGroup = styled(InputGroup)`
+  margin-bottom: 24px;
+  width: 100%;
+`;
+
+export const LineWrap = styled.div`
+  padding: 0 0 24px;
+`;
--- src/app/space-ship/Modal/ManageShip/index.tsx
+++ src/app/space-ship/Modal/ManageShip/index.tsx
@@ -1,17 +1,54 @@
 import { ModalType } from "@/@types";
 import BoxModal from "@/components/common/BoxModal";
 import * as S from "./index.styled";
-import { ButtonGroup, FillButton, OutlineButton } from "@/components/Account/common.styled";
+import {
+  ButtonGroup,
+  FillButton,
+  InputGroup,
+  Label,
+  OutlineButton,
+  TextArea,
+} from "@/components/Account/common.styled";
+import Input from "@/components/common/Input";
+import Line from "@/components/common/Line";
+import AdjustBtnInput from "@/components/common/AdjustBtnInput";
 
 interface Type extends ModalType {}
 
-export default function CreateShipModal({ onClose }: Type) {
+export default function ManageShip({ onClose }: Type) {
   return (
     <BoxModal onClose={onClose} title="새 우주선 만들기">
-      <S.Notification></S.Notification>
+      <S.Notification>
+        <InputGroup>
+          <Input label="우주선 이름" />
+        </InputGroup>
+        <InputGroup>
+          <Label>우주선 설명</Label>
+          <TextArea $height="96px" />
+        </InputGroup>
+        <S.Center>
+          <S.AdjustBtnGroup>
+            <AdjustBtnInput label="인원수" />
+          </S.AdjustBtnGroup>
+          <S.AdjustBtnGroup>
+            <AdjustBtnInput label="탑승 인원수" />
+          </S.AdjustBtnGroup>
+        </S.Center>
+        <S.Center>
+          <S.AdjustBtnGroup>
+            <AdjustBtnInput label="탑승 인원수" />
+          </S.AdjustBtnGroup>
+          <S.AdjustBtnGroup>
+            <AdjustBtnInput label="탑승 인원수" />
+          </S.AdjustBtnGroup>
+        </S.Center>
+        <S.LineWrap>
+          <Line color="gray" />
+        </S.LineWrap>
+      </S.Notification>
       <ButtonGroup>
         <OutlineButton>취소</OutlineButton>
-        <FillButton>수정 완료</FillButton>
+        <FillButton>작성 완료</FillButton>
       </ButtonGroup>
     </BoxModal>
   );
