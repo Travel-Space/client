efb34dcbaaa825846b839b3e986edcdcc9d4516c
joayeon <codinz.n@gmail.com>
2023-10-17 21:53:03 +0900

    feat: line props 추가

--- src/components/common/Line/index.tsx
+++ src/components/common/Line/index.tsx
@@ -2,7 +2,7 @@ import * as S from "./index.styled";
 
 export interface colorProp {
   color: "gray" | "white";
-  size: "vertical" | "horizontal";
+  size: "vertical" | "horizontal" | "shortVertical";
 }
 
 export default function Line({ color, size }: colorProp) {
