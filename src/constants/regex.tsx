c9c775f83985757988b89f60fbe9ea9f1dadd810
helloleesul <55569192+helloleesul@users.noreply.github.com>
2023-10-23 05:32:14 +0900

    feat: Validate 정규식 email, onlyNumber 추가

--- src/constants/regex.tsx
+++ src/constants/regex.tsx
@@ -1,6 +1,8 @@
 const VALIDATE = {
   name: /^[가-힣]{2,10}$/,
   nickName: /^(?=.*[a-z0-9가-힣])[a-z0-9가-힣\s]{2,10}$/,
+  email: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+  onlyNumber: /[^0-9]/g,
 };
 
 export default VALIDATE;
