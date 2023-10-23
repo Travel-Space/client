5fba62255b7455d49ceaece182c6da0f9b4d43a8
[SW5] 문수민 <sum95771@gmail.com>
2023-10-24 06:05:37 +0900

    feat: 행성모양 상수파일 추가

---
+++ src/constants/planetShape.tsx
@@ -0,0 +1,10 @@
+type ObjType = {
+  [index: string]: string;
+};
+const PLANETSHAPE: ObjType = {
+  SHAPE1: "/assets/img/icons/planet-1.svg",
+  SHAPE2: "/assets/img/icons/planet-2.svg",
+  SHAPE3: "/assets/img/icons/planet-3.svg",
+};
+
+export default PLANETSHAPE;
