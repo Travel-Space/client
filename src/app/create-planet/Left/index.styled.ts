13e2f9646fc3623c90cad29bc6476f058fa99b10
helloleesul <55569192+helloleesul@users.noreply.github.com>
2023-10-26 03:31:11 +0900

    design: 해시태그 버튼 디자인 수정

--- src/app/create-planet/Left/index.styled.ts
+++ src/app/create-planet/Left/index.styled.ts
@@ -67,7 +67,7 @@ export const TagGroup = styled.ul`
 export const Tag = styled.li`
   ${flexAlignCenter}
   background: ${({ theme }) => theme.PALETTE.white};
-  padding: 8px 16px;
+  padding: 8px 14px 8px 16px;
   border-radius: 100px;
   gap: 8px;
   span {
@@ -78,5 +78,6 @@ export const Tag = styled.li`
     width: 12px;
     height: 12px;
     background-image: url("/assets/img/icons/close-mini.svg");
+    margin-bottom: 1px;
   }
 `;
