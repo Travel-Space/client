7c992e0ce307944103badebfbcaf1e2c804f04c1
[SW5] 문수민 <sum95771@gmail.com>
2023-10-26 23:22:01 +0900

    feat: api 공통코드 수정

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosHeaders } from "axios";
+import axios from "axios";
 
 const allowMethod: string[] = ["get", "post", "put", "patch", "delete"];
 
@@ -8,10 +8,6 @@ axios.defaults.headers.post["Content-Type"] = "application/json";
 
 axios.defaults.withCredentials = true;
 axios.defaults.timeout = 5000;
-
-// axios 인스턴스화 - 따로 만들어야 수정하기도 편함
-// export const api1 = axios.create({ baseURL: "http://api1" });
-// export const api2 = axios.create({ baseURL: "http://api2" });
 
 // TODO: interceptor 적용
 // 요청 보내다가 취소해야되는 경우에도 사용함 / 서버 다 끊어버릴때 사용. 헤더값에 키 심어놓고 이 키를 변수로 놓음.
@@ -37,7 +33,7 @@ axios.defaults.timeout = 5000;
 
 // 정의된 함수 시그니처에 맞게 인터페이스 생성
 interface AxiosRequest {
-  requestAxios: <T>(method: string, url: string, data?: {}, headers?: {}) => Promise<T>;
+  requestAxios: <T>(method: string, url: string, data?: {}) => Promise<T>;
 }
 
 const axiosRequest: AxiosRequest = {
@@ -51,7 +47,7 @@ const axiosRequest: AxiosRequest = {
    * @param url 호출 url 작성. path param은 url에 같이 정의해준다.
    * @param data request body에 해당하는 사항. post, put 시 추가/수정할 객체를 지정해주면 된다. get은 빈 객체를 보낸다.
    */
-  requestAxios: async <T>(method: string, url: string, data = {}, headers = {}) => {
+  requestAxios: async <T>(method: string, url: string, data = {}) => {
     // 이상한 method 넣으면 실행 못하게 미리 에러 처리 한다.
     if (!allowMethod.includes(method.toLowerCase())) throw new Error("허용되지 않은 호출 method입니다.");
     try {
@@ -59,15 +55,12 @@ const axiosRequest: AxiosRequest = {
         method,
         url: `${axios.defaults.baseURL}${url}`,
         data,
-        headers, // 아이템, 인벤토리 호출할때만 넣기. 주요 기능에만 제한하는 것은 어떨지
       });
 
-      return response.data as T;
+      return response as T;
     } catch (error) {
       //TODO: 에러 처리를 클라이언트에서 바로 할 수 있도록 구성 해야 한다.
-      // react error boundary?
-      // 클라이언트에서 직접 응답을 처리할 수 있도록 axios 훅을 만들면 좋을 것 같다.
-      // 리액트 쿼리, SWR
+      // react error boundary
       console.log(error);
       throw error;
     }
