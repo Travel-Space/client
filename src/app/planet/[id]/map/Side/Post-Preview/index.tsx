9768486ab8d89abb3bdd70551cc5c0b2f3a703bf
joayeon <codinz.n@gmail.com>
2023-10-27 20:22:12 +0900

    feat: map 동적 라우팅 설정 및 게시글 불러오는 로직 추가

---
+++ src/app/planet/[id]/map/Side/Post-Preview/index.tsx
@@ -0,0 +1,26 @@
+import * as S from "./index.styled";
+import UserProfile from "@/components/common/UserProfile";
+import { ArticleProps } from "..";
+
+const previewImg = "/assets/img/icons/post-test-img.svg";
+
+export default function PostPreview({ article }: ArticleProps) {
+  const { id, title, content, createdAt, author } = article;
+
+  return (
+    <S.Post id={id}>
+      <S.MainBox>
+        <UserProfile size="chat" author={author} />
+
+        <S.Description>
+          <span>{title}</span>
+          <div>{content}</div>
+        </S.Description>
+
+        <S.Date>{createdAt}</S.Date>
+      </S.MainBox>
+
+      <S.PreviewImg src={previewImg} alt="프리뷰 이미지" />
+    </S.Post>
+  );
+}
--- src/app/planet/[id]/map/page.tsx
+++ src/app/planet/[id]/map/page.tsx
@@ -1,21 +1,26 @@
 "use client";
 
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
+
+import axiosRequest from "@/api";
+import { Posting } from "@/@types/Posting";
 
 import * as S from "./page.styled";
 import Side from "./Side";
-import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
 
 const containerStyle = {
   width: "100%",
   height: "calc(100vh - 90px)",
 };
 
+// 임시 중점
 const center = {
   lat: 37.5465029,
   lng: 127.065263,
 };
 
+// 임시 좌표 : 마커 찍을 위치
 const locations = [
   { place: "성수낙낙", lat: 37.5465029, lng: 127.065263 },
   { place: "왕십리역", lat: 37.561949, lng: 127.038485 },
@@ -23,13 +28,17 @@ const locations = [
   { place: "코엑스", lat: 37.5116828, lng: 127.059151 },
 ];
 
-export default function Map() {
+export default function Map({ params }: { params: { id: number } }) {
+  // side bar open
   const [isOpen, setIsOpen] = useState(false);
 
-  const handleClickSide = () => {
-    setIsOpen(prev => !prev);
-  };
+  // google map
+  const [map, setMap] = useState<google.maps.Map | null>(null);
 
+  // 게시글 정보
+  const [article, setArticle] = useState([]);
+
+  // google map key
   const googleMapsApiKey = process.env.NEXT_PUBLIC_GOOGLE_MAP_KEY || "";
 
   const { isLoaded } = useJsApiLoader({
@@ -37,7 +46,26 @@ export default function Map() {
     googleMapsApiKey,
   });
 
-  const [map, setMap] = useState<google.maps.Map | null>(null);
+  // 게시글 정보 받아오는 api
+  const getArticle = async () => {
+    try {
+      const response = await axiosRequest.requestAxios<Posting[]>("get", `/articles/byPlanet?planetId=${params.id}`);
+      const data = response.data;
+
+      setArticle(data);
+    } catch (error) {
+      console.error("에러 발생: ", error);
+    }
+  };
+
+  useEffect(() => {
+    getArticle();
+  }, []);
+
+  // side bar open
+  const handleClickSide = () => {
+    setIsOpen(prev => !prev);
+  };
 
   const handleMount = useCallback(function callback(map: any) {
     setMap(null);
@@ -53,18 +81,9 @@ export default function Map() {
     setMap(map);
   }, []);
 
-  // const Marker = () => {
-  //   return (
-  //     <S.Marker>
-  //       <S.Images src="https://cdn.pixabay.com/photo/2020/09/09/02/12/smearing-5556288_1280.jpg" />
-  //       <S.Markers src="/assets/img/icons/marker.svg" />
-  //     </S.Marker>
-  //   );
-  // };
-
   return (
     <S.Container>
-      {isOpen && <Side onClose={handleClickSide} />}
+      {isOpen && <Side onClose={handleClickSide} article={article} />}
 
       <S.Button onClick={handleClickSide}>→</S.Button>
 
@@ -76,21 +95,6 @@ export default function Map() {
           onLoad={handleMarkerLoad}
           onUnmount={handleMount}
         >
-          {/* {locations.map((location, index) => (
-            <CustomMarker key={index} lat={location.lat} lng={location.lng} />
-          ))} */}
-          {/* {locations.map((location, index) => (
-            <MarkerF
-              key={index}
-              position={{ lat: location.lat, lng: location.lng }}
-              icon={{
-                url: "/assets/img/icons/marker.svg",
-                scaledSize: new window.google.maps.Size(40, 40),
-              }}
-            >
-              <S.Images src="https://cdn.pixabay.com/photo/2020/09/09/02/12/smearing-5556288_1280.jpg" />
-            </MarkerF>
-          ))} */}
           {locations.map((location, index) => (
             <Marker
               key={index}
@@ -106,6 +110,37 @@ export default function Map() {
       )}
     </S.Container>
   );
+}
+
+// 주석
+
+// const Marker = () => {
+//   return (
+//     <S.Marker>
+//       <S.Images src="https://cdn.pixabay.com/photo/2020/09/09/02/12/smearing-5556288_1280.jpg" />
+//       <S.Markers src="/assets/img/icons/marker.svg" />
+//     </S.Marker>
+//   );
+// };
+
+{
+  /* {locations.map((location, index) => (
+            <CustomMarker key={index} lat={location.lat} lng={location.lng} />
+          ))} */
+}
+{
+  /* {locations.map((location, index) => (
+            <MarkerF
+              key={index}
+              position={{ lat: location.lat, lng: location.lng }}
+              icon={{
+                url: "/assets/img/icons/marker.svg",
+                scaledSize: new window.google.maps.Size(40, 40),
+              }}
+            >
+              <S.Images src="https://cdn.pixabay.com/photo/2020/09/09/02/12/smearing-5556288_1280.jpg" />
+            </MarkerF>
+          ))} */
 }
 
 // interface MarkerProp {
