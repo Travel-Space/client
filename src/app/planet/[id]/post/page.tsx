af8205e08f5adc97d2686ef2d504ef147be63a5a
wook888 <guddnr888@gmail.com>
2023-10-28 02:33:42 +0900

    feat: 게시글 좋아요 기능 구현 게시글에 접속한 유저의 id와 게시글에 좋아요한 id 비교

--- src/app/planet/[id]/post/page.tsx
+++ src/app/planet/[id]/post/page.tsx
@@ -7,10 +7,10 @@ import { ResData, Posting } from "@/@types/index";
 import { useRecoilValue } from "recoil";
 import { userAtom } from "@/recoil/atoms/user.atom";
 import { AxiosResponse } from "axios";
+import { useSearchParams } from "next/navigation";
 import PostContent from "./detail/PostContent";
 import LikeAndShare from "./detail/LikeAndShare";
 import CommentList from "./detail/CommentList";
-import { useSearchParams } from "next/navigation";
 
 interface PostDetailProps {
   data?: Posting;
@@ -19,9 +19,11 @@ interface PostDetailProps {
 export default function PostDetail() {
   const params = useSearchParams();
   const post = params.get('detail');
-  
+
+
   const [data, setData] = useState<Posting | null>(null);
-  const [likedStatus, setLikedStatus] = useState(false);
+  const [likedStatus, setLikedStatus] = useState<boolean | null>(null);
+
   const currentUser = useRecoilValue(userAtom);
 
   // 게시글 본문 fetch get 함수
@@ -31,8 +33,10 @@ export default function PostDetail() {
       setData(response.data);
 
       // 현재 로그인한 사용자가 좋아요를 눌렀는지 확인
-      const isLikedByCurrentUser = response.data.likes.some(like => like.userId === currentUser.id);
+      const isLikedByCurrentUser = response.data.likes.some(likes => likes.userId === currentUser.id);
       setLikedStatus(isLikedByCurrentUser);
+      console.log(currentUser.id);
+      console.log(isLikedByCurrentUser);
     } catch (error) {
       alert("게시글 정보를 가져오는 중 에러가 발생했습니다. 다시 시도해주세요.");
       console.error("Error fetching profile data: ", error);
@@ -45,7 +49,7 @@ export default function PostDetail() {
 
   if (!data) return <div>Loading...</div>;
 
-  // 게시글 좋아요 Post 함수
+  // 게시글 좋아요, 좋아요 취소 함수
   async function handleLikeAction() {
     try {
       let response: AxiosResponse;
@@ -55,11 +59,13 @@ export default function PostDetail() {
       } else {
         response = await axiosRequest.requestAxios("post", `/articles/${post}/like`, {});
       }
+      setLikedStatus(prevStatus => !prevStatus);
     } catch (error) {
       console.error("Error with like action: ", error);
-      alert(likedStatus ? "좋아요 취소에 실패하였습니다." : "좋아요에 실패하였습니다.");
+      alert(likedStatus ? "이미 좋아요한 게시글입니다." : "좋아요에 실패하였습니다.");
     }
   }
+
 
   return (
     <PD.Wrapper>
