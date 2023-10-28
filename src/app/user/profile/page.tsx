40541859de2cedd680e5dc7d95f3f4095eba51be
[SW5] 문수민 <sum95771@gmail.com>
2023-10-29 01:57:48 +0900

    feat: 내 행성정보 recoil 적용

--- src/app/user/profile/page.tsx
+++ src/app/user/profile/page.tsx
@@ -1,6 +1,11 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import * as S from "./page.styled";
+import axiosRequest from "@/api/index";
+import { ResData, Planet } from "@/@types/index";
+import { useRecoilState } from "recoil";
+import myPlanetsState from "@/recoil/atoms/myPlanets.atom";
+import Planets from "@/app/user/profile/Planets";
 
 import ProfileSummary from "./ProfileSummary";
 import Line from "@/components/common/Line";
@@ -11,6 +16,24 @@ export default function Profile({ children }: { children: React.ReactNode }) {
   const selectTab = (idx: number) => {
     setTabIndex(idx);
   };
+
+  //행성 불러오기
+  const [planets, setPlanets] = useRecoilState(myPlanetsState);
+  async function getPlanets() {
+    try {
+      const response = await axiosRequest.requestAxios<ResData<Planet[]>>("get", "/planet/my-planets");
+      setPlanets(response.data);
+      console.log("planets", planets);
+    } catch (error) {
+      alert("행성 정보를 가져오는중 에러가 발생했습니다. 다시 시도해주세요.");
+      console.error("Error fetching planet data: ", error);
+    }
+  }
+  useEffect(() => {
+    // console.log("planets", planets);
+    if (planets.length === 0) getPlanets();
+  }, []);
+
   const TabList = [
     {
       title: "팔로워",
@@ -38,15 +61,18 @@ export default function Profile({ children }: { children: React.ReactNode }) {
     },
     {
       title: "행성목록",
-      content: (
-        <Nothing
-          src="/assets/img/icons/no-planets.svg"
-          alt="no-TravelingPlanets"
-          width={148}
-          height={148}
-          comment="여행 중인 행성이 없습니다."
-        />
-      ),
+      content:
+        planets.length === 0 ? (
+          <Nothing
+            src="/assets/img/icons/no-planets.svg"
+            alt="no-TravelingPlanets"
+            width={148}
+            height={148}
+            comment="여행 중인 행성이 없습니다."
+          />
+        ) : (
+          <Planets data={planets} />
+        ),
     },
     {
       title: "게시글",
@@ -80,6 +106,7 @@ export default function Profile({ children }: { children: React.ReactNode }) {
           ))}
         </S.TabListWrap>
         <Line color="gray" size="horizontal" />
+
         <S.MainContent>
           {TabList[tabIndex].content}
           {children}
