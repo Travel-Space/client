9614d0f4148742dd2a667fe0e8ea0974e152cde6
wook888 <guddnr888@gmail.com>
2023-10-29 21:36:29 +0900

    feat: 게시글 조회 데이터 조회 및 댓글 조회

---
+++ src/app/planet/[id]/post/detail/CommentList/CommentWrite/index.styled.ts
@@ -0,0 +1,20 @@
+import { flexColumn } from "@/styles/common";
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+${flexColumn}
+  width: 100%;
+  height: 100%;
+  gap: 24px;
+`;
+
+export const BtnDisplay = styled.div`
+  display: flex;
+  justify-content: right;
+`;
+
+export const CommentButton = styled.button`
+  width: 140px;
+  height: 100%;
+  background: ${({ theme }) => theme.PALETTE.white};
+`;
