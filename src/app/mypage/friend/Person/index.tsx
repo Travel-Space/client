a1c4c80286f8fa81bcb4afcc98b7af738bbe85d6
[SW5] 문수민 <sum95771@gmail.com>
2023-10-31 05:04:58 +0900

    feat: 팔로우 버튼 공통컴포넌트 분리

--- src/app/mypage/friend/Person/index.tsx
+++ src/app/mypage/friend/Person/index.tsx
@@ -1,87 +1,15 @@
-import axiosRequest from "@/api";
-import { ResData, Follower, Following, User } from "@/@types";
-
-import { useState } from "react";
-import { useRecoilState } from "recoil";
-import { followerState, followingState } from "@/recoil/atoms/friend.atom";
+import { User } from "@/@types";
 
 import * as S from "./index.styled";
 
 import Image from "next/image";
-import Button from "@/components/common/Button";
+import FollowBtn from "../FollowBtn";
 
 interface PersonProps {
   data: User;
   isMutual?: boolean;
 }
 export default function Person({ data, isMutual }: PersonProps) {
-  const [followState, setFollowState] = useState(isMutual === false ? "unfollowing" : "following");
-
-  const [followers, setFollowers] = useRecoilState(followerState);
-  const [followings, setFollowings] = useRecoilState(followingState);
-  //팔로우 하기
-  async function follow(userId: number) {
-    try {
-      const response = await axiosRequest.requestAxios<ResData<Follower[]>>("post", `/user/follow/${userId}`);
-      //response로 팔로잉목록 받기 - 수정예정
-      // setFollowings(response.data);
-      setFollowState("following");
-      console.log("follow", response.data);
-    } catch (error) {
-      alert("팔로우 요청 중 에러가 발생했습니다. 다시 시도해주세요.");
-      console.error("Error posting follow: ", error);
-    }
-  }
-  //언팔로우 하기
-  async function unfollow(userId: number) {
-    try {
-      const response = await axiosRequest.requestAxios<ResData<Follower[]>>("delete", `/user/unfollow/${userId}`);
-      // console.log("userId", userId);
-
-      //response로 팔로잉목록 받기 - 수정예정
-      // setFollowings(response.data);
-      setFollowState("unfollowing");
-    } catch (error) {
-      alert("팔로우 취소 요청 중 에러가 발생했습니다. 다시 시도해주세요.");
-      console.error("Error posting unfollow: ", error);
-    }
-  }
-  //팔로워 조회
-  //response 적용 후 - 수정예정
-  async function getFollowers() {
-    try {
-      const response = await axiosRequest.requestAxios<ResData<Follower[]>>("get", `/user/followers`);
-      setFollowers(response.data);
-      console.log("followers", response.data);
-    } catch (error) {
-      alert("팔로워 정보를 가져오는중 에러가 발생했습니다. 다시 시도해주세요.");
-      console.error("Error fetching followers data: ", error);
-    }
-  }
-  //팔로잉 조회
-  //response 적용 후 - 수정예정
-  async function getFollowings() {
-    try {
-      const response = await axiosRequest.requestAxios<ResData<Following[]>>("get", `/user/following`);
-      setFollowings(response.data);
-      // console.log("followers", response.data);
-    } catch (error) {
-      alert("팔로잉 정보를 가져오는중 에러가 발생했습니다. 다시 시도해주세요.");
-      console.error("Error fetching followings data: ", error);
-    }
-  }
-  const handleClickFollow = async (userId: number) => {
-    await follow(userId);
-    await getFollowers();
-    console.log(isMutual);
-    getFollowings();
-  };
-  const handleClickUnfollow = async (userId: number) => {
-    await unfollow(userId);
-    console.log(isMutual);
-    getFollowings();
-    getFollowers();
-  };
   return (
     <S.Container>
       <div>
@@ -91,17 +19,7 @@ export default function Person({ data, isMutual }: PersonProps) {
           <S.Email>{data.email}</S.Email>
         </S.Info>
       </div>
-      <S.FollowBtn>
-        {followState === "following" ? (
-          <Button variant="reverse" shape="medium" size="smallWithXsFont" onClick={() => handleClickUnfollow(data.id)}>
-            언팔로우
-          </Button>
-        ) : (
-          <Button variant="confirm" shape="medium" size="smallWithXsFont" onClick={() => handleClickFollow(data.id)}>
-            팔로우
-          </Button>
-        )}
-      </S.FollowBtn>
+      <FollowBtn userId={data.id} isMutual={isMutual} />
     </S.Container>
   );
 }
--- src/app/mypage/friend/add/RecommendFriend/index.tsx
+++ src/app/mypage/friend/add/RecommendFriend/index.tsx
@@ -3,24 +3,17 @@ import { Follower } from "@/@types";
 import * as S from "./index.styled";
 
 import Image from "next/image";
-import Button from "@/components/common/Button";
+import FollowBtn from "../../FollowBtn";
 
 interface RecommendFriendProps {
   data: Follower;
 }
 export default function RecommendFriend({ data }: RecommendFriendProps) {
-  const handleClick = () => {
-    console.log();
-  };
   return (
     <S.Container>
       <Image src="/assets/img/icons/default-user.svg" alt="default-user" width={76} height={76} />
       <S.Nickname>{data.user.name}</S.Nickname>
-      <S.FollowBtn>
-        <Button variant="confirm" shape="medium" size="smallWithXsFont" onClick={handleClick}>
-          팔로우
-        </Button>
-      </S.FollowBtn>
+      <FollowBtn userId={data.userId} isMutual={data.isMutual} />
     </S.Container>
   );
 }
