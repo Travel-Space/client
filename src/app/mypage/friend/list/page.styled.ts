31942fab13265c533119aab03cada3cf6ebabf7e
[SW5] 문수민 <sum95771@gmail.com>
2023-10-30 15:43:38 +0900

    feat: 친구목록 페이지 레이아웃 삭제

--- src/app/mypage/friend/list/page.styled.ts
+++ src/app/mypage/friend/list/page.styled.ts
@@ -1,7 +1,55 @@
 import { styled } from "styled-components";
-import { flexCenter } from "@/styles/common";
+import { flexCenter, flexColumn } from "@/styles/common";
 
 export const Container = styled.div`
+  width: 100%;
+  min-height: 846px;
+  background-color: ${({ theme }) => theme.PALETTE.white};
+  border-radius: 10px;
+`;
+
+export const Header = styled.header`
+  background-color: ${({ theme }) => theme.PALETTE.white};
+  border: 1px solid ${({ theme }) => theme.PALETTE.gray[100]};
+  border-radius: 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 32px 42px;
+
+  & > div {
+    display: flex;
+    gap: 24px;
+    height: 43px;
+  }
+`;
+export const FollowerNumber = styled.div`
+  ${flexColumn}
+`;
+
+export const FollowingNumber = styled.div`
+  ${flexColumn}
+`;
+
+export const Title = styled.div`
+  &:hover {
+    color: ${({ theme }) => theme.PALETTE.mainColor};
+  }
+  &:focus {
+    color: ${({ theme }) => theme.PALETTE.mainColor};
+  }
+  font-size: ${({ theme }) => theme.FONT_SIZE.md};
+  font-weight: 500;
+  margin-bottom: 8px;
+`;
+
+export const Number = styled.div`
+  font-size: ${({ theme }) => theme.FONT_SIZE.big};
+  font-weight: 700;
+`;
+
+export const MainContainer = styled.div`
+  padding: 32px 0 0;
   border-radius: 10px;
 `;
 
--- src/app/mypage/friend/list/page.tsx
+++ src/app/mypage/friend/list/page.tsx
@@ -1,36 +1,63 @@
 "use client";
+import { useState } from "react";
 
 import * as S from "./page.styled";
 
+import SearchForm from "@/app/mypage/SearchForm";
+import Line from "@/components/common/Line";
 import Nothing from "@/components/common/Nothing";
 import Person from "@/app/mypage/friend/Person";
 
-export default function Planet() {
+export default function FriendList() {
+  const [selectedMenu, setSelectedMenu] = useState("닉네임");
+  const dropDownProps = {
+    menuList: ["닉네임", "계정"],
+    selectedMenu: selectedMenu, //선택한 메뉴
+    handleClick: setSelectedMenu, //메뉴를 클릭했을 때 실행될 메서드
+    placeholder: "친구 목록에서 검색합니다.",
+  };
+
   return (
     <S.Container>
-      <Nothing
-        src="/assets/img/icons/no-friends.svg"
-        alt="no-friends"
-        width={216}
-        height={216}
-        comment="등록된 친구가 없습니다."
-        suggest="닉네임 또는 계정을 검색해 보세요."
-        font="lg"
-      />
+      <S.Header>
+        <div>
+          <S.FollowerNumber>
+            <S.Title>팔로워</S.Title>
+            <Number>102</Number>
+          </S.FollowerNumber>
+          <Line color="gray" size="vertical" />
+          <S.FollowingNumber>
+            <S.Title>팔로잉</S.Title>
+            <Number>3,888</Number>
+          </S.FollowingNumber>
+        </div>
+        <SearchForm select={dropDownProps} />
+      </S.Header>
+      <S.MainContainer>
+        <Nothing
+          src="/assets/img/icons/no-friends.svg"
+          alt="no-friends"
+          width={216}
+          height={216}
+          comment="등록된 친구가 없습니다."
+          suggest="닉네임 또는 계정을 검색해 보세요."
+          font="lg"
+        />
 
-      <S.MyFriends>
-        <Person />
-        <Person />
-        <Person />
-        <Person />
-        <Person />
-        <Person />
-        <Person />
-        <Person />
-        <Person />
-        <Person />
-      </S.MyFriends>
-      <S.ShowMoreBtn>목록 더보기</S.ShowMoreBtn>
+        <S.MyFriends>
+          <Person />
+          <Person />
+          <Person />
+          <Person />
+          <Person />
+          <Person />
+          <Person />
+          <Person />
+          <Person />
+          <Person />
+        </S.MyFriends>
+        <S.ShowMoreBtn>목록 더보기</S.ShowMoreBtn>
+      </S.MainContainer>
     </S.Container>
   );
 }
