3555c511f44757c8f3c6d5c00865d510322730c6
joayeon <codinz.n@gmail.com>
2023-11-04 01:38:22 +0900

    feat: 행성 롤에 따라서 상단 부분 변경 로직 추가

--- src/app/planet/[id]/map/Side/index.tsx
+++ src/app/planet/[id]/map/Side/index.tsx
@@ -1,9 +1,12 @@
 import { createPortal } from "react-dom";
 import Link from "next/link";
 import { useState } from "react";
+import { useRecoilValue } from "recoil";
+import { usePathname } from "next/navigation";
 
 import { Posting } from "@/@types/Posting";
 import { ArticleProps } from "../page";
+import { userAtom } from "@/recoil/atoms/user.atom";
 
 import PlanetInfo from "./Planet-Info";
 import PostPreview from "./Post-Preview";
@@ -13,7 +16,56 @@ import DropDown from "@/components/common/DropDown";
 import Nothing from "@/components/common/Nothing";
 
 export default function Side({ onClose, article, params }: ArticleProps) {
+  const { isAuth, memberships } = useRecoilValue(userAtom);
+
+  const pathname = usePathname();
+  const planetId = Number(pathname.split("/")[2]);
+
+  const role = {
+    OWNER: {
+      roles: "관리자",
+      tag: ["행성 관리", "우주선"],
+      link: [`/planet/${planetId}/modify`, `/planet/${planetId}/space-ship`],
+    },
+    ADMIN: {
+      roles: "부관리자",
+      tag: "행성 관리",
+      link: `/planet/${planetId}/modify`,
+    },
+    MEMBER: {
+      roles: "일반",
+      tag: "우주선",
+      link: `/planet/${planetId}/space-ship`,
+    },
+    GUEST: {
+      roles: "게스트",
+      tag: "행성 탑승",
+    },
+  };
+
   const [selectedMenu, setSelectedMenu] = useState("전체");
+
+  const memberCheck = memberships.planets.find(el => el?.planetId === planetId);
+
+  const checkJoinPlanet = () => {
+    if (memberCheck === undefined) return false;
+    return true;
+  };
+
+  console.log(memberships);
+
+  const roleCheck = () => {
+    // 게스트일 경우, 게스트 / 행성 탑승, 관리자 / 행성 관리, 우주선, 일반 / 우주선
+    if (memberCheck?.role === "OWNER") {
+      return role.OWNER;
+    } else if (memberCheck?.role === "ADMIN") {
+      return role.ADMIN;
+    } else if (memberCheck?.role === "MEMBER") {
+      return role.MEMBER;
+    } else return role.GUEST;
+  };
+
+  roleCheck();
 
   const dropDownProps = {
     comment: "", //미선택시 보여질 문구(필요할 때만 추가)
@@ -27,16 +79,19 @@ export default function Side({ onClose, article, params }: ArticleProps) {
       {createPortal(
         <S.Container>
           <S.Wrapper>
-            <PlanetInfo />
+            <PlanetInfo role={roleCheck()} />
 
             <div>
               <S.Middle>
                 <div>
                   <DropDown color="none" font="md" shape="round" props={dropDownProps} />
                 </div>
-                <Link href={`/planet/${params}/post/write`}>
-                  <S.Button>새 글 작성</S.Button>
-                </Link>
+
+                {isAuth && checkJoinPlanet() && (
+                  <Link href={`/planet/${params}/post/write`}>
+                    <S.Button>새 글 작성</S.Button>
+                  </Link>
+                )}
               </S.Middle>
 
               <S.ScrollBox>
@@ -48,7 +103,9 @@ export default function Side({ onClose, article, params }: ArticleProps) {
                       width={60}
                       height={60}
                       comment="현재 작성된 게시글이 없습니다."
-                      suggest="먼저 게시글을 작성해 보세요."
+                      suggest={
+                        checkJoinPlanet() ? "먼저 게시글을 작성해 보세요." : "행성에 가입한 후 글을 작성해 보세요."
+                      }
                       font="sm"
                     />
                   )}
