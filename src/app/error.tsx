4781c16b92531018a17d5580081d155dbe283940
joayeon <codinz.n@gmail.com>
2023-11-05 17:22:30 +0900

    design: 에러 페이지 디자인 수정

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -3,19 +3,24 @@
 import Link from "next/link";
 import styled from "styled-components";
 
-import { bodyContainer, flexCenter, flexColumnCenter } from "@/styles/common";
+import { bodyContainer, flexAlignCenter, flexCenter, flexColumnCenter } from "@/styles/common";
 
 export default function Error() {
   return (
     <>
       <Container>
+        <img src="/assets/img/icons/error/error5.png" />
+
         <Comment>
-          <div>에러가 발생했습니다!</div>
-          <div>요청하신 페이지를 찾을 수 없습니다.</div>
+          <div>문제가 발생했습니다!</div>
+          <div>다시 메인으로 돌아가 주세요!</div>
         </Comment>
 
         <Button>
-          <Link href={"/"}>Go To Home</Link>
+          <Link href={"/"}>
+            <div>Click</div>
+            <img src="/assets/img/icons/error/click.svg" />
+          </Link>
         </Button>
       </Container>
     </>
@@ -30,23 +35,36 @@ export const Container = styled.div`
 
   height: calc(100vh - 90px);
   color: ${({ theme }) => theme.PALETTE.white};
+
+  > img {
+    width: 280px;
+  }
 `;
 
 export const Comment = styled.div`
-  line-height: 1.3;
+  line-height: 1.5;
   ${flexColumnCenter}
 
   font-size: ${({ theme }) => theme.FONT_SIZE.lg};
+  font-weight: 600;
 `;
 
-export const Button = styled.button`
-  padding: 10px 24px;
-  border-radius: 10px;
-  background-color: ${({ theme }) => theme.PALETTE.primary[400]};
+export const Button = styled.div`
+  padding: 8px 10px 8px 20px;
+  border-radius: 50px;
+  background: none;
+  border: 2px solid ${({ theme }) => theme.PALETTE.white};
+  /* background-color: ${({ theme }) => theme.PALETTE.primary[400]}; */
   font-size: ${({ theme }) => theme.FONT_SIZE.em};
+  font-weight: 600;
 
   > a {
+    ${flexCenter}
     color: ${({ theme }) => theme.PALETTE.white};
     text-decoration: none;
+
+    > img {
+      width: 30px;
+    }
   }
 `;
