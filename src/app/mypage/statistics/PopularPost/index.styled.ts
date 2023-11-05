b379b166026c9fefbe34eb11a497652cd7855031
[SW5] 문수민 <sum95771@gmail.com>
2023-11-05 18:42:38 +0900

    design: 인기글 랭킹 디자인 수정

--- src/app/mypage/statistics/PopularPost/index.styled.ts
+++ src/app/mypage/statistics/PopularPost/index.styled.ts
@@ -2,17 +2,16 @@ import { styled } from "styled-components";
 
 export const Table = styled.table`
   width: 100%;
-  margin-top: 32px;
+  margin-top: 40px;
 `;
 export const Tablebody = styled.tbody`
+  border-top: 1px solid ${({ theme }) => theme.PALETTE.gray[100]};
+
   & > tr:nth-child(1) > td {
     padding-top: 14px;
   }
 `;
 export const TableHeader = styled.thead`
-  margin-bottom: 8px;
-  border-bottom: 1px solid ${({ theme }) => theme.PALETTE.gray[100]};
-
   & > th {
     padding-bottom: 8px;
     font-size: ${({ theme }) => theme.FONT_SIZE.sm};
@@ -22,14 +21,17 @@ export const TableHeader = styled.thead`
 export const TdCenter = styled.th`
   text-align: center;
   width: 120px;
+  font-size: ${({ theme }) => theme.FONT_SIZE.sm};
+  font-weight: 500;
 `;
 export const TdLeft = styled.th`
   text-align: left;
 `;
 export const TdTitle = styled.th`
   text-align: center;
-
+  white-space: nowrap;
   & > div {
+    padding-bottom: 8px;
     font-size: ${({ theme }) => theme.FONT_SIZE.md};
     font-weight: 600;
   }
