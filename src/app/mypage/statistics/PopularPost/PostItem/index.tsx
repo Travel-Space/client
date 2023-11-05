e280fbef5140694bb225be030dea135c6237a0ec
[SW5] 문수민 <sum95771@gmail.com>
2023-11-06 07:15:18 +0900

    feat: 인기 게시글 데이터 바인딩

--- src/app/mypage/statistics/PopularPost/PostItem/index.tsx
+++ src/app/mypage/statistics/PopularPost/PostItem/index.tsx
@@ -1,18 +1,24 @@
+import { Posting } from "@/@types";
+
 import * as S from "./index.styled";
+
+import { getDateFormatWithDay } from "@/utils/getDateFormat";
 
 interface PostItemProps {
   ranking: number;
+  data: Posting;
 }
-export default function PostItem({ ranking }: PostItemProps) {
+export default function PostItem({ ranking, data }: PostItemProps) {
+  const formattedDate = getDateFormatWithDay(data.createdAt).slice(0, 10);
+
   return (
     <S.TableRow>
       <S.Ranking>{ranking}</S.Ranking>
       <S.TdLeft>
-        <div>일본여행 재밌음(제 1탄)</div>
+        <div>{data.title}</div>
       </S.TdLeft>
-      <S.TdCenter>234</S.TdCenter>
-      <S.TdCenter>일본 맛도리 여행</S.TdCenter>
-      <S.TdCenter>2023.10.02</S.TdCenter>
+      <S.TdCenter>{data.monthlyViews}</S.TdCenter>
+      <S.TdCenter>{formattedDate}</S.TdCenter>
     </S.TableRow>
   );
 }
