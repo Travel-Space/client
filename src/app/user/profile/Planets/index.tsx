5dd408f4a312bce6cfb569a91b51f69c24f65edf
[SW5] 문수민 <sum95771@gmail.com>
2023-11-07 15:53:23 +0900

    feat: 유저 프로필 소유행성 추가

--- src/app/user/profile/Planets/index.tsx
+++ src/app/user/profile/Planets/index.tsx
@@ -1,5 +1,5 @@
 import axiosRequest from "@/api/index";
-import { ResData, JoinedPlanets, Planet } from "@/@types";
+import { ResData, JoinedPlanets, Planet, PlanetsType } from "@/@types";
 
 import { useState, useEffect, useRef } from "react";
 
@@ -9,15 +9,32 @@ import PlanetItem from "@/components/User/PlanetItem";
 import useInfiniteScroll from "@/hooks/useInfiniteScroll";
 import Nothing from "@/components/common/Nothing";
 import MESSAGE from "@/constants/message";
+import MyPlanet from "@/app/mypage/MyPlanet";
 
 const Planets = ({ id }: { id: number }) => {
+  const [userPlanets, setUserPlanets] = useState<Planet[]>([]);
   const [planets, setPlanets] = useState<Planet[]>([]);
   const [totalCount, setTotalCount] = useState(0);
 
   const [page, setPage] = useState(1);
   const [disableLoadData, setDisableLoadDate] = useState(false);
 
-  //여행중인 행성 불러오기
+  //소유 행성 조회
+  async function getUserPlanets() {
+    try {
+      const response = await axiosRequest.requestAxios<ResData<PlanetsType>>(
+        "get",
+        `/planet/other/${id}/ownerships?page=1&limit=5`,
+      );
+      const planets = response.data.data;
+
+      setUserPlanets(planets);
+    } catch (error) {
+      console.error("행성 정보를 가져오는중 에러가 발생했습니다.", error);
+      alert(MESSAGE.ERROR.DEFAULT);
+    }
+  }
+  //여행중인 행성 조회
   async function getPlanets() {
     try {
       const response = await axiosRequest.requestAxios<ResData<JoinedPlanets>>(
@@ -58,31 +75,43 @@ const Planets = ({ id }: { id: number }) => {
 
   useEffect(() => {
     getPlanets();
+    getUserPlanets();
     // console.log("planets", planets);
   }, []);
 
   return (
     <S.Container>
+      <S.UserPlanets>
+        <S.Title>소유 행성</S.Title>
+        <S.Planets>
+          {userPlanets.map((planet, idx) => (
+            <MyPlanet key={idx} data={planet} />
+          ))}
+        </S.Planets>
+      </S.UserPlanets>
+      <S.Header>
+        <S.Title>여행 중인 행성</S.Title>
+        <S.Number>
+          총 <span>{totalCount}</span>개의 행성
+        </S.Number>
+      </S.Header>
       {!!totalCount ? (
-        <>
-          <S.Number>
-            총 <span>{totalCount}</span>개의 게시글
-          </S.Number>
-          <S.Content>
-            {planets.map((planet, idx) => (
-              <PlanetItem key={`user-planet${idx}`} data={planet} userId={id} />
-            ))}
-          </S.Content>
-        </>
+        <S.Content>
+          {planets.map((planet, idx) => (
+            <PlanetItem key={`user-planet${idx}`} data={planet} userId={id} />
+          ))}
+        </S.Content>
       ) : (
-        <Nothing
-          src="/assets/img/icons/no-planets.svg"
-          alt="no-TravelingPlanets"
-          width={148}
-          height={148}
-          comment="여행 중인 행성이 없습니다."
-          font="lg"
-        />
+        <S.Content>
+          <Nothing
+            src="/assets/img/icons/no-planets.svg"
+            alt="no-TravelingPlanets"
+            width={148}
+            height={148}
+            comment="여행 중인 행성이 없습니다."
+            font="lg"
+          />
+        </S.Content>
       )}
       <S.InfiniteScrollTarget ref={observerRef} />
     </S.Container>
