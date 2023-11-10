c5a9a17c281388c7b06c5fbf4f6dbb66765ba7d4
helloleesul <55569192+helloleesul@users.noreply.github.com>
2023-11-11 05:20:29 +0900

    feat: 행성멤버 아닐 시 페이지 접근 제한 추가

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -3,14 +3,22 @@ import { useRouter } from "next/navigation";
 import { useRecoilValue } from "recoil";
 import { userAtom } from "@/recoil/atoms/user.atom";
 
-export function useAuth() {
+export function useAuth(planetId?: number) {
   const user = useRecoilValue(userAtom);
   const router = useRouter();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
-    if (user) {
-      setIsLoggedIn(true);
+    if (user && user.memberships && user.memberships.planets) {
+      const hasPlanetId = user.memberships.planets.some(
+        membership => membership.planetId === planetId && membership.role !== "GUEST",
+      );
+
+      if (hasPlanetId) {
+        setIsLoggedIn(true);
+      } else {
+        router.push("/");
+      }
     } else {
       router.push("/");
     }
