9c34c866435c6b0abde585c619b3462a99b0d2bc
joayeon <codinz.n@gmail.com>
2023-11-12 23:56:43 +0900

    fix: 채팅 멤버 보여주는 모달 수정

--- src/app/chat/Content/Modal/index.tsx
+++ src/app/chat/Content/Modal/index.tsx
@@ -1,19 +1,12 @@
+import { useRecoilValue } from "recoil";
 import * as S from "./index.styled";
 import Line from "@/components/common/Line";
+import { userAtom } from "@/recoil/atoms/user.atom";
 
-const name = [
-  { name: "수민수민", role: "우주선 방장" },
-  { name: "현규현규", role: "일반" },
-  { name: "은선은선", role: "일반" },
-  { name: "혜진혜진", role: "일반" },
-  { name: "형욱형욱", role: "일반" },
-  { name: "이슬이슬", role: "일반" },
-  { name: "아연아연", role: "일반" },
-  { name: "보란보란", role: "일반" },
-  { name: "하은하은", role: "일반" },
-];
+export default function Modal(members: []) {
+  const user = useRecoilValue(userAtom);
+  const me = members.members.filter(el => el.nickname === user?.nickName);
 
-export default function Modal() {
   return (
     <S.ModalBox>
       <S.Title>
@@ -23,18 +16,18 @@ export default function Modal() {
 
       <S.ProfileBox>
         <S.Profile>
-          <img src="/assets/img/icons/profileImage.png" />
-          <span>남경남경 (나)</span>
+          <img src={me[0]?.profileImage} />
+          <span>{me[0]?.nickname} (나)</span>
         </S.Profile>
 
         <Line size="horizontal" color="gray" />
 
-        {name.map(el => (
+        {members.members.map(el => (
           <S.Profile>
-            <img src="/assets/img/icons/profileImage.png" />
+            <img src={el.profileImage} />
             <span>
-              {el.name}
-              {el.role === "우주선 방장" ? <strong>우주선 방장</strong> : ""}
+              {el.nickname}
+              {el.role === "OWNER" ? <strong>방장</strong> : ""}
             </span>
           </S.Profile>
         ))}
