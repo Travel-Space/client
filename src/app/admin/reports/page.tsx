373022cdc7f7caa8ca272154608d0972bdef3af2
[SW5] 예은선 <ysvln0912@gmail.com>
2023-11-12 16:15:06 +0900

    feat: 관리자 신고 관리 제작

--- src/app/admin/reports/page.tsx
+++ src/app/admin/reports/page.tsx
@@ -1,14 +1,17 @@
 "use client";
-import * as S from "../admin.styled";
 import axiosRequest from "@/api/index";
+import { ResData } from "@/@types/index";
 import { useState, useEffect } from "react";
-import { Report, ResData } from "@/@types/Report";
+import * as S from "../admin.styled";
+
+import { Report } from "@/@types/Report";
 import { Select, Button, Space } from "antd";
 
 import TotalText from "../TotalText";
 import SearchBar from "../SearchBar";
 import ReportNameModal from "../Modal/ReportNameModal";
 import ReportAcceptModal from "../Modal/ReportAcceptModal";
+import ReportDetailModal from "../Modal/ReportDetailModal";
 import AdminTable from "../Table";
 
 export default function Reports() {
@@ -17,55 +20,83 @@ export default function Reports() {
   const [isOpen, setIsOpen] = useState({
     reportName: false,
     reportReason: false,
+    reportDetail: false,
   });
 
-  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(10);
+  const [currentPage, setCurrentPage] = useState(1);
   const pageSize = 10;
 
-  useEffect(() => {
-    async function getReports() {
-      try {
-        const response = await axiosRequest.requestAxios<ResData<Report[]>>(
-          "get",
-          `/reports?page=${page}&pageSize=${pageSize}`,
-        );
-        setReportsData(response.data.reports);
-      } catch (error) {
-        alert("오류");
+  const [filterName, setFilterName] = useState("");
+  const [filterEmail, setFilterEmail] = useState("");
+  const [filterStatus, setFilterStatus] = useState<string>("");
+
+  const getReports = async () => {
+    try {
+      let apiUrl = `/reports/search?page=${currentPage}&pageSize=${pageSize}`;
+      if (filterName) {
+        apiUrl += `&name=${filterName}`;
       }
+      if (filterEmail) {
+        apiUrl += `&email=${filterEmail}`;
+      }
+      if (filterStatus !== "") {
+        apiUrl += `&status=${filterStatus}`;
+      }
+
+      const response = await axiosRequest.requestAxios<ResData<Report>>("get", apiUrl);
+      setReportsData(response.data.reports);
+      console.log("신고", response.data);
+      setTotal(response.data.totalCount);
+    } catch (error) {
+      alert("오류");
     }
+  };
+
+  useEffect(() => {
     getReports();
-  }, []);
-  console.log(reportsData);
+  }, [currentPage]);
+
+  useEffect(() => {
+    setCurrentPage(1);
+    getReports();
+  }, [filterName, filterEmail, filterStatus]);
 
   const getReportDetails = async (id: number) => {
     try {
-      const response = await axiosRequest.requestAxios<ResData<Report[]>>("get", `/reports/${id}`);
+      const response = await axiosRequest.requestAxios<ResData<Report>>("get", `/reports/${id}`);
       openReportModal(response.data);
-      console.log(response);
+      console.log(response, "GetReportDetail");
     } catch (error) {
       console.error("특정 신고 내용을 불러오는 중 에러 발생:", error);
     }
   };
 
-  const onViewArticle = (id: number) => {
-    console.log(`자세히 보기: ${id}`);
+  const onViewReportDetails = async (id: number) => {
+    try {
+      const response = await axiosRequest.requestAxios<ResData<Report>>("get", `/reports/${id}`);
+      setSelectedReport(response.data);
+      setIsOpen(() => ({ reportReason: false, reportName: false, reportDetail: true }));
+    } catch (error) {
+      console.error("특정 신고 내용을 불러오는 중 에러 발생:", error);
+    }
   };
 
   const openReportModal = (report: Report) => {
     setSelectedReport(report);
-    setIsOpen(() => ({ reportReason: false, reportName: true }));
+    setIsOpen(() => ({ reportReason: false, reportName: true, reportDetail: false }));
   };
 
-  console.log(isOpen);
+  const onPageChange = (page: number) => {
+    setCurrentPage(page);
+  };
 
   const getStatusLabel = (status: string) => {
     switch (status) {
       case "RECEIVED":
-      case "UNDER_REVIEW":
         return "검토 중";
       case "APPROVED":
-        return "요청 승낙";
+        return "요청 수락";
       case "REJECTED":
         return "요청 거절";
       default:
@@ -87,20 +118,20 @@ export default function Reports() {
     },
     {
       title: "신고자",
-      dataIndex: "reporterId" as keyof Report,
-      key: "reporterId",
+      dataIndex: ["reporter", "name"] as (keyof Report)[],
+      key: "reporter.name",
     },
     {
       title: "이메일",
-      dataIndex: "email" as keyof Report,
-      key: "email",
+      dataIndex: ["reporter", "email"] as (keyof Report)[],
+      key: "reporter.email",
     },
     {
       title: "신고내용",
       key: "reason",
       render: (record: Report) => (
         <Space size="middle">
-          <Button type="link" onClick={() => getReportDetails(record.id)}>
+          <Button type="link" onClick={() => getReportDetails(record.id)} disabled={record.status !== "RECEIVED"}>
             자세히 보기
           </Button>
         </Space>
@@ -108,11 +139,16 @@ export default function Reports() {
     },
     {
       title: (
-        <Select defaultValue="처리 상태" style={{ width: 100 }}>
+        <Select
+          defaultValue={getStatusLabel(filterStatus)}
+          style={{ width: 100 }}
+          value={filterStatus}
+          onChange={value => setFilterStatus(value)}
+        >
           <Select.Option value="">전체</Select.Option>
-          <Select.Option value="검토 중">검토 중</Select.Option>
-          <Select.Option value="요청 거절">요청 거절</Select.Option>
-          <Select.Option value="요청 수락">요청 수락</Select.Option>
+          <Select.Option value="RECEIVED">검토 중</Select.Option>
+          <Select.Option value="REJECTED">요청 거절</Select.Option>
+          <Select.Option value="APPROVED">요청 수락</Select.Option>
         </Select>
       ),
       dataIndex: "status" as keyof Report,
@@ -124,7 +160,7 @@ export default function Reports() {
       key: "view",
       render: (record: Report) => (
         <Space size="middle">
-          <Button type="link" onClick={() => onViewArticle(record.reporterId)}>
+          <Button type="link" onClick={() => onViewReportDetails(record.id)} disabled={record.status == "RECEIVED"}>
             자세히 보기
           </Button>
         </Space>
@@ -137,12 +173,28 @@ export default function Reports() {
       <S.TableContainer>
         <S.AdminContent>
           <S.TopContent>
-            <TotalText titleText={"신고"} totalNum={reportsData.length} unit={"개"} />
-            <SearchBar />
+            <TotalText titleText={"신고"} totalNum={total} unit={"개"} />
+            <SearchBar
+              searchType="reports"
+              onSearch={data => {
+                setFilterName(data.name);
+                setFilterEmail(data.email);
+              }}
+            />
           </S.TopContent>
-          <AdminTable data={reportsData} columns={columns} />
+
+          <AdminTable
+            data={reportsData}
+            columns={columns}
+            currentPage={currentPage}
+            itemsPerPage={pageSize}
+            total={total}
+            onPageChange={onPageChange}
+          />
+
           {isOpen.reportName && <ReportNameModal report={selectedReport} setIsOpen={setIsOpen} />}
-          {isOpen.reportReason && <ReportAcceptModal report={selectedReport} />}
+          {isOpen.reportReason && <ReportAcceptModal report={selectedReport} setIsOpen={setIsOpen} />}
+          {isOpen.reportDetail && <ReportDetailModal report={selectedReport} setIsOpen={setIsOpen} />}
         </S.AdminContent>
       </S.TableContainer>
     </S.AdminLayout>
