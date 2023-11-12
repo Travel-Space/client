55c6b17759e7e5a620592f17d5edd489909d047f
[SW5] 예은선 <ysvln0912@gmail.com>
2023-11-12 16:13:23 +0900

    feat: 신고 처리 상세 내용 모달 제작

---
+++ src/app/admin/Modal/ReportDetailModal/index.styled.ts
@@ -0,0 +1,65 @@
+import { flexSpaceBetweenCenter, flexAlignCenter } from "@/styles/common";
+import styled from "styled-components";
+
+export const Content = styled.div`
+  width: 352px;
+
+  padding-top: 32px;
+  ul > li {
+    margin-bottom: 16px;
+  }
+
+  ul > li > span {
+    display: block;
+    margin-bottom: 8px;
+    font-weight: 700;
+    font-size: ${({ theme }) => theme.FONT_SIZE.md};
+  }
+
+  p {
+    font-size: ${({ theme }) => theme.FONT_SIZE.sm};
+    font-weight: 500;
+    color: ${({ theme }) => theme.PALETTE.gray[200]};
+  }
+`;
+
+export const ImgBox = styled.div`
+  background-color: #ccc;
+  width: 100%;
+  height: 146px;
+  text-align: center;
+  ${flexSpaceBetweenCenter}
+  margin-bottom: 24px;
+  cursor: pointer;
+  > img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export const Button = styled.button`
+  width: 100%;
+  padding: 8px;
+  background-color: ${({ theme }) => theme.PALETTE.black};
+  font-size: ${({ theme }) => theme.FONT_SIZE.xs};
+  font-weight: 700;
+  color: ${({ theme }) => theme.PALETTE.white};
+`;
+
+export const ButtonContainer = styled.div`
+  ${flexSpaceBetweenCenter}
+  gap: 16px;
+`;
+
+export const ButtonAccept = styled(Button)`
+  border-radius: 5px;
+  color: ${({ theme }) => theme.PALETTE.white};
+  background-color: ${({ theme }) => theme.PALETTE.mainColor};
+  margin-bottom: 0;
+`;
+
+export const ButtonRefuse = styled(ButtonAccept)`
+  background-color: ${({ theme }) => theme.PALETTE.white};
+  color: ${({ theme }) => theme.PALETTE.mainColor};
+  border: 2px solid ${({ theme }) => theme.PALETTE.mainColor};
+`;
