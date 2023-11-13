31965e02da750215ac2de17bfa32012bc52e8679
helloleesul <55569192+helloleesul@users.noreply.github.com>
2023-11-13 17:28:29 +0900

    feat: baseURL /api 엔드포인트 추가

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,7 +5,7 @@ const allowMethod: string[] = ["get", "post", "put", "patch", "delete"];
 
 // Axios 인스턴스 생성
 const instance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "http://localhost:8080/api",
   timeout: 10000,
   withCredentials: true,
 });
