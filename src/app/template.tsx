90b69ac3954f994e7b3638b38e9a2aad532db507
helloleesul <55569192+helloleesul@users.noreply.github.com>
2023-11-16 17:48:15 +0900

    fix: template컴포넌트 에러 수정

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import Header from "@/components/layout/Header";
-import ChatBtn from "@/components/common/ChatBtn";
 import GlobalStyles from "@/styles/global";
 import theme from "@/styles/theme";
 import { ThemeProvider } from "styled-components";
 import { RecoilRoot } from "recoil";
+
+import dynamic from "next/dynamic";
+
+const Header = dynamic(() => import("@/components/layout/Header"), { ssr: false });
+const ChatBtn = dynamic(() => import("@/components/common/ChatBtn"), { ssr: false });
 
 export default function Template({ children }: { children: React.ReactNode }) {
   // pathname === chat 일 경우 채팅 버튼 삭제
