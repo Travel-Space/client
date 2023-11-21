f6b07a6e315ab875348194f0c5b28a7965da0ed2
helloleesul <55569192+helloleesul@users.noreply.github.com>
2023-11-22 01:25:29 +0900

    refactor: Account 코드 정리, enum 삭제

--- src/components/Account/index.tsx
+++ src/components/Account/index.tsx
@@ -1,55 +1,56 @@
 import { useState } from "react";
+import { Default } from "@/@types/Modal";
 
+import * as ACCOUNT from "./index.styled";
 import Side from "@/components/common/Side";
 import Login from "./Login";
 import Signup from "./Signup";
 import ResetPassword from "./ResetPassword";
 
-import * as S from "./index.styled";
-import { Default } from "@/@types/Modal";
+const ACCOUNT_PAGE = {
+  LOGIN: "Log in",
+  SIGNUP: "Sign up",
+  RESET_PW: "Reset password",
+} as const;
 
-enum Page {
-  Login = "Log in",
-  Signup = "Sign up",
-  ResetPassword = "Reset password",
-}
+type ACCOUNT_PAGE = (typeof ACCOUNT_PAGE)[keyof typeof ACCOUNT_PAGE];
 
 export default function Account({ onClose }: Default) {
-  const [currentPage, setCurrentPage] = useState<Page>(Page.Login);
+  const [currentPage, setCurrentPage] = useState<ACCOUNT_PAGE>(ACCOUNT_PAGE.LOGIN);
 
   return (
     <Side>
-      <S.Buttons $right={currentPage === Page.Login}>
-        {currentPage !== Page.Login && (
-          <S.Button className="prev" onClick={() => setCurrentPage(Page.Login)}>
+      <ACCOUNT.Buttons $right={currentPage === ACCOUNT_PAGE.LOGIN}>
+        {currentPage !== ACCOUNT_PAGE.LOGIN && (
+          <ACCOUNT.Button className="prev" onClick={() => setCurrentPage(ACCOUNT_PAGE.LOGIN)}>
             뒤로
-          </S.Button>
+          </ACCOUNT.Button>
         )}
-        <S.Button className="close" onClick={onClose}>
+        <ACCOUNT.Button className="close" onClick={onClose}>
           닫기
-        </S.Button>
-      </S.Buttons>
-      <S.Wrap>
-        <S.Title>
+        </ACCOUNT.Button>
+      </ACCOUNT.Buttons>
+      <ACCOUNT.Wrap>
+        <ACCOUNT.Title>
           <p>
             {currentPage} to
             <br />
             Travel Space
           </p>
-        </S.Title>
+        </ACCOUNT.Title>
 
-        {currentPage === Page.Login ? (
+        {currentPage === ACCOUNT_PAGE.LOGIN ? (
           <Login
-            goToResetPassword={() => setCurrentPage(Page.ResetPassword)}
-            goToSignup={() => setCurrentPage(Page.Signup)}
+            goToResetPassword={() => setCurrentPage(ACCOUNT_PAGE.RESET_PW)}
+            goToSignup={() => setCurrentPage(ACCOUNT_PAGE.SIGNUP)}
             onClose={onClose}
           />
-        ) : currentPage === Page.Signup ? (
-          <Signup goToLogin={() => setCurrentPage(Page.Login)} />
-        ) : currentPage === Page.ResetPassword ? (
-          <ResetPassword goToLogin={() => setCurrentPage(Page.Login)} />
+        ) : currentPage === ACCOUNT_PAGE.SIGNUP ? (
+          <Signup goToLogin={() => setCurrentPage(ACCOUNT_PAGE.LOGIN)} />
+        ) : currentPage === ACCOUNT_PAGE.RESET_PW ? (
+          <ResetPassword goToLogin={() => setCurrentPage(ACCOUNT_PAGE.LOGIN)} />
         ) : null}
-      </S.Wrap>
+      </ACCOUNT.Wrap>
     </Side>
   );
 }
