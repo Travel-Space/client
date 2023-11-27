824a5c0bdebe81077a3551aeeba761543692f85e
helloleesul <55569192+helloleesul@users.noreply.github.com>
2023-11-27 23:38:48 +0900

    fix: Side Background 클릭 이벤트 선택적 추가

--- src/components/common/Side/index.tsx
+++ src/components/common/Side/index.tsx
@@ -1,6 +1,8 @@
 import * as S from "./index.styled";
 import { createPortal } from "react-dom";
 
-export default function Side({ children }: { children: React.ReactNode }) {
-  return <S.Background>{createPortal(<S.Container>{children}</S.Container>, document.body)}</S.Background>;
+export default function Side({ children, onClick }: { children: React.ReactNode; onClick?: () => {} }) {
+  return (
+    <S.Background onClick={onClick}>{createPortal(<S.Container>{children}</S.Container>, document.body)}</S.Background>
+  );
 }
