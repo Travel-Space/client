86ff5e2aecb3c9102328f4b7244f9f4c12acb158
[SW5] 문수민 <sum95771@gmail.com>
2024-02-08 00:38:30 +0900

    refactor: 오타 수정 및 불필요한 코드 삭제

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,17 +9,10 @@ export const metadata: Metadata = {
   },
   // 오픈 그래프 설정
   openGraph: {
-    title: "Travle Space: 트래블 스페이스",
+    title: "Travel Space: 트래블 스페이스",
     description: "다녀온 여행을 기록하는 나만의 공간",
     url: "https://travelspace.world/",
     siteName: "Travel Space",
-    images: [
-      {
-        url: "",
-        width: 0,
-        height: 0,
-      },
-    ],
   },
 };
 
